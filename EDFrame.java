0951c45c74760480e42d923f89e8634ed4fcdf3e
Avinash Dengani <avinashdengani123@gmail.com>
2022-10-04 02:32:45 +0530

    add: made different panels for encryption and decryption

--- EDFrame.java
+++ EDFrame.java
@@ -1,12 +1,26 @@
 import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.BorderLayout;
 
 public class EDFrame extends Frame{
-    
+    Panel encryptionPanel;
+    Panel decryptionPanel;
+
     public EDFrame(String title) {
         super(title);
         setSize(800, 600);
         setLocationRelativeTo(null);
-
+        setLayout(new GridLayout());
         addWindowListener(new EDWindowClosing(this));
+
+        this.encryptionPanel = new Panel(new BorderLayout());
+        this.decryptionPanel = new Panel(new BorderLayout());
+
+        this.add(this.encryptionPanel);
+        this.add(this.decryptionPanel);
+
+        new Encryption(this, this.encryptionPanel);
+        new Decryption(this, this.decryptionPanel);
     }
 }
